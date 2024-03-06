6bcf417bdfcac249145678be8ffea01519234a4d
janeffer945 <janeffernjeri945@gmail.com>
2024-03-06 11:04:55 +0300

    variables in java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+
+// public class Hello{
+//     public static void main(String[] args){
+//         System.out.println("hi the///poiuytr");
+//     }
+// }
+    class Hello{
+    public static void main(String[] args){
+     int num1 = 9;
+     int num2 = 19;
+     int result = num1 + num2; 
+
+    System.out.println(result);
+    }
+    }
