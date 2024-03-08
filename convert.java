4e40d94f52167f7dc1379ea592c9ee8c7cab63d1
janeffer945 <janeffernjeri945@gmail.com>
2024-03-08 14:16:32 +0300

    conversions

--- convert.java
+++ convert.java
@@ -2,7 +2,10 @@ public class convert {
     public static  void main(String[] args){
        byte b= 124;
        int a = b;
-       System.out.println( a); 
+       int k = 125;
+       byte n = (byte)k;
+
+       System.out.println( n); 
     }
     
 }
