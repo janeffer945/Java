9c30480019bb4761e7ed96ae2683fd620f7d491a
janeffer945 <janeffernjeri945@gmail.com>
2024-03-08 14:30:33 +0300

    increment and decrement operators

--- convert.java
+++ convert.java
@@ -4,8 +4,12 @@ public class convert {
        int a = b;
        int k = 125;
        byte n = (byte)k;
+       int num = 8;
+       num = num +1;
+       num ++; //increment
+       num += 1; //decrement
 
-       System.out.println( n); 
+       System.out.println( num); 
     }
     
 }
