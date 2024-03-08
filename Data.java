3f9d4be8eb57b029491927ec9958212483cb83dd
janeffer945 <janeffernjeri945@gmail.com>
2024-03-08 13:07:04 +0300

    literals exponentiation 12e10 which is 12*10^10

--- Data.java
+++ Data.java
@@ -1,13 +1,14 @@
 public class Data {
     public static void main(String[] args){
-        int num1 = 9;
-        long l = 5897l;
-        byte by = 124; 
-        short sh = 78;
-        float f = 4.9f;
-        boolean b = false;
-        double d = 4.9;  
-        char c = 'k';
+       double num1 = 12e10;
+        System.out.println(num1);
+        // long l = 5897l;
+        // byte by = 124; 
+        // short sh = 78;
+        // float f = 4.9f;
+        // boolean b = false;
+        // double d = 4.9;  
+        // char c = 'k';
 
 
          
--- Hello.java
+++ Hello.java
@@ -4,12 +4,13 @@
 //         System.out.println("hi the///poiuytr");
 //     }
 // }
+
     class Hello{
     public static void main(String[] args){
      int num1 = 9;
      int num2= 19;
      int result = num1 + num2;
-
     System.out.println(result);
     }
+    
     }
