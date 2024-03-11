cc42f0beab09ef33a48dde4d4f3f744b17c2aeaf
janeffer945 <janeffernjeri945@gmail.com>
2024-03-11 10:21:11 +0300

    relational operetors

---
+++ relation.java
@@ -0,0 +1,9 @@
+public class relation {
+    public static void main(String[] args){
+        int y = 8;
+        int m = 10;
+        boolean results = y>=m;
+        System.out.println(results);
+
+    }
+}
