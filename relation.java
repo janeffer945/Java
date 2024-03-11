297c1157af580dcef002217cddd09d7893feafdd
janeffer945 <janeffernjeri945@gmail.com>
2024-03-11 11:29:50 +0300

    logical operators

--- relation.java
+++ relation.java
@@ -1,8 +1,13 @@
 public class relation {
     public static void main(String[] args){
-        int y = 8;
-        int m = 10;
-        boolean results = y>=m;
+        // int y = 11;
+        // int m = 10;
+        // boolean results = y!=m;
+         int a = 10;
+         int b = 9;
+         int c = 8;
+         int d = 5;
+         boolean results = a>b || c<d;
         System.out.println(results);
 
     }
