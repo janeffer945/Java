dc90bbdd4ad8ae46c89999bd83bccb9b01c4245c
janeffer945 <janeffernjeri945@gmail.com>
2024-03-11 11:51:22 +0300

    conditional statements

---
+++ State.java
@@ -0,0 +1,11 @@
+public class State {
+    public static void main(String[] args){
+        int x = 8;
+        if (x>12 && x<10) 
+
+            System.out.println("hello");
+            System.out.println( "bye");
+            
+        
+    }
+}
