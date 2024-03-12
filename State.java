f801f40eb24d4249486b03a82d00ab806d820184
janeffer945 <janeffernjeri945@gmail.com>
2024-03-12 09:34:07 +0300

    Update: if else if conditions

--- State.java
+++ State.java
@@ -1,11 +1,18 @@
 public class State {
     public static void main(String[] args){
-        int x = 8;
-        if (x>12 && x<10) 
+        // int x = 8;
+        // if (x>12 && x<10) 
 
-            System.out.println("hello");
-            System.out.println( "bye");
-            
-        
+        //     System.out.println("hello");
+        //     System.out.println( "bye");
+            int x = 10;
+            int y = 18;
+            int z = 21;
+            if (x>y && x>z)
+            System.out.println(x);
+            else if (y>x && y>z)
+            System.out.println(y);
+            else 
+            System.out.println(z);
     }
 }
