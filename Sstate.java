332616cc051159931a8cf7cc073b511350aa2654
janeffer945 <janeffernjeri945@gmail.com>
2024-03-12 12:32:25 +0300

    switch statements

---
+++ Sstate.java
@@ -0,0 +1,12 @@
+public class Sstate{
+    public static void main(String[] args){
+        int i = 1;
+        int k = 4;       
+
+        if (i>k)
+        System.out.println("monday");
+         else if (i<k)
+        System.out.println("tuesday");
+        
+    }
+}
