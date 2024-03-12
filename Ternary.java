0458d980b112f5b225bf6f610bab40bd7db1fd85
janeffer945 <janeffernjeri945@gmail.com>
2024-03-12 09:49:59 +0300

    ternary checking if an integer or odd number

---
+++ Ternary.java
@@ -0,0 +1,13 @@
+public class Ternary {
+    public static void main(String[] args){
+        int k = 10;
+        if(k%2 ==0) {
+            System.out.println(10);
+        }
+        else 
+        System.out.println(30);
+
+    }
+    
+}
+
