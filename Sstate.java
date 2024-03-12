e6664e6b942d2af56e8ac45fe979eb43ea3da433
janeffer945 <janeffernjeri945@gmail.com>
2024-03-12 12:51:46 +0300

    swich statements

--- Sstate.java
+++ Sstate.java
@@ -7,16 +7,20 @@ public class Sstate{
         // System.out.println("monday");
         //  else if (i<k)
         // System.out.println("tuesday");
-        int i = 1;
+        int i = 5;
         switch (i) {
             case 1:
                 System.out.println("monday");
+               break;
                 case 2:
                 System.out.println("tuesday");
+                 break;
                 case 3:
                 System.out.println("wednesday");
+                 break;
                 case 4:
                 System.out.println("thursday"); 
+                 break;
                 case 5: 
                 System.out.println("friday");
                                                 
