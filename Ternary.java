a7800d0404009204b54b0086809f5f0aaccc480a
janeffer945 <janeffernjeri945@gmail.com>
2024-03-12 12:20:14 +0300

    clean code

--- Ternary.java
+++ Ternary.java
@@ -1,11 +1,14 @@
 public class Ternary {
     public static void main(String[] args){
-        int k = 10;
-        if(k%2 ==0) {
-            System.out.println(10);
-        }
-        else 
-        System.out.println(30);
+        int k = 11;
+         int result = 0;
+         result = k%2==0 ? 10 : 20;
+         System.out.println(result);
+        // if(k%2 ==0) {
+        //     System.out.println(10);
+        // }
+        // else 
+        // System.out.println(30);
 
     }
     
