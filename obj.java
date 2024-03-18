a3b6a44c3c8cedc78207b6ef771fe37d5e8b5b47
janeffer945 <janeffernjeri945@gmail.com>
2024-03-18 17:43:32 +0300

    for loop

---
+++ obj.java
@@ -0,0 +1,18 @@
+// class calculator{
+//     int x = 4;
+//     int c = 7;
+
+
+// }
+
+
+ class obj {
+    public static void main(String a[]){
+        int x = 100;
+        for (x=0; x<= 100; x++ ) {
+                   System.out.println("sorry " + x);
+ 
+        }
+
+    } 
+}
