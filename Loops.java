f8b5d53ae42f95fee78533106b12c6eaca3fe78c
janeffer945 <janeffernjeri945@gmail.com>
2024-03-18 11:23:13 +0300

    Do and while loop

--- Loops.java
+++ Loops.java
@@ -1,12 +1,33 @@
-public class Loops {
-    public static void main(String[] args){
-        int i = 5;
-     while (i>=3) {
-        System.out.println("hello " + i);
-       
-        i--;
+// public class Loops {
+//     public static void main(String[] args) {
+//         int i = 5;
+//         while (i >= 4 && i <= 8) {
+//             System.out.println("hi " + i);
+//             int y = 3;
+//             while(y<=5){
+//                 System.out.println("hey " + y);
+//                 y++;
+//             }
+
+//             i++;
+//         }
+//         System.out.println("bye5");
+//     }
+// }
+public class Loops{
+    public static void main (String[] args){
+
+        int r  = 5;
         
-     }
+        do{ 
+           System.out.println("hello " + r);
+            --r;
+
+        }
+        while(r>=4 );
+        {
+            System.out.println("hi " + r);
+          
+        }
     }
-    
 }
