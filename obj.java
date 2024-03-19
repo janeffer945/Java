c1e56ca47b4ccb7c360b48f1aa808edd1770c012
janeffer945 <janeffernjeri945@gmail.com>
2024-03-19 22:59:25 +0300

    class and object practical

--- obj.java
+++ obj.java
@@ -1,18 +1,31 @@
-// class calculator{
-//     int x = 4;
-//     int c = 7;
+class calculator{
+        // int result = sumc + sumx;
+public int add(){
+    
+        System.out.println("Hello");
+        return 0;
+    }
+    
 
-
-// }
-
-
- class obj {
-    public static void main(String a[]){
-        int x = 100;
-        for (x=0; x<= 100; x++ ) {
-                   System.out.println("sorry " + x);
- 
-        }
-
-    } 
 }
+class obj{
+    public static void  main (String a[] ){
+int sumx = 4;
+    int sumc = 7;
+    calculator calc = new calculator();
+    calc.add();
+
+    }
+}
+
+
+//  class obj {
+//     public static void main(String a[]){
+//         int x = 100;
+//         for (x=0; x<= 100; x++ ) {
+//                    System.out.println("sorry " + x);
+ 
+//         }
+
+//     } 
+// }
