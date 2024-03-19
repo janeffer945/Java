d03b0c8d691ed50f9ba0f75058b4b491af6e91f0
janeffer945 <janeffernjeri945@gmail.com>
2024-03-19 23:43:26 +0300

    for loop

--- method.java
+++ method.java
@@ -3,11 +3,27 @@ public class method {
     public static void main(String a[] ){
         computer comp = new computer();
         comp.music ();
+        String str  = comp.getpen( 10);
+        System.out.println(str);
+        
     }
 }
  
 class computer{
     public void music(){
         System.out.println("cool music plaiying");
+    }
+
+    public String getpen(int cost){
+        if (cost<=10) {
+            
+            return" nothing";
+        }
+        else{
+            return"available";
+        }
+
+        
+
     }
 }
