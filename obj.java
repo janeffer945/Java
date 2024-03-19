66151da7d7f9fec144fc8a773a99aabe2bf463f6
janeffer945 <janeffernjeri945@gmail.com>
2024-03-19 23:50:11 +0300

    for loop

--- obj.java
+++ obj.java
@@ -1,31 +1,48 @@
-class calculator{
-        // int result = sumc + sumx;
-public int add(int sumc7, int sumx4){
-    int result = sumc7 + sumx4; 
+// class calculator{
+//         // int result = sumc + sumx;
+// public int add(int sumc7, int sumx4){
+//     int result = sumc7 + sumx4; 
     
-        return result;
-    }   
+//         return result;
+//     }   
 
-}
-class obj{
-    public static void  main (String a[] ){
-int sumx = 4;
-    int sumc = 7;
-    calculator calc = new calculator();
-   int result = calc.add(7, 4);
-   System.out.println(result);
+// }
+// class obj{
+//     public static void  main (String a[] ){
+// int sumx = 4;
+//     int sumc = 7;
+//     calculator calc = new calculator();
+//    int result = calc.add(7, 4);
+//    System.out.println(result);
+
+//     }
+// }
+
+
+// //  class obj {
+// //     public static void main(String a[]){
+// //         int x = 100;
+// //         for (x=0; x<= 100; x++ ) {
+// //                    System.out.println("sorry " + x);
+ 
+// //         }
+
+// //     } 
+// // }
+/**
+ * obj
+ */
+
+
+ class calculator{
+    public int  add(int n1, int n2 ){
+return n1+ n2;
+
+    }
+ }
+public class obj {
+
+    public static void main(String a[]){
 
     }
 }
-
-
-//  class obj {
-//     public static void main(String a[]){
-//         int x = 100;
-//         for (x=0; x<= 100; x++ ) {
-//                    System.out.println("sorry " + x);
- 
-//         }
-
-//     } 
-// }
